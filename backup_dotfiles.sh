4cbbe9bfac62ccab2db4e1cc86ed5cf1a14d6047
Nocookieforu <aicantarxd@gmail.com>
2016-03-26 17:28:17 -0500

    Removed dotfiles copy in backup script and updated README.

--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -17,21 +17,3 @@ pacman -Qqen > $CONF_DIR/packages/arch_pkglist_$DATE_STR.txt
 # Overwrite main package list to store with git (dated ones are ignored)
 cp $CONF_DIR/packages/arch_pkglist_$DATE_STR.txt $CONF_DIR/packages/arch_pkglist.txt
 
-# Back up all configurations in dotfiles
-# Bash
-cp ~/.profile       $CONF_DIR/bash/profile
-cp ~/.bashrc        $CONF_DIR/bash/bashrc
-cp ~/.bash_profile  $CONF_DIR/bash/bash_profile
-cp ~/.bash_logout   $CONF_DIR/bash/bash_logout
-
-# X
-cp ~/.xinitrc       $CONF_DIR/x11/xinitrc
-cp ~/.Xresources    $CONF_DIR/x11/Xresources
-
-# Git
-cp ~/.gitconfig     $CONF_DIR/git/gitconfig
-
-# Vim
-cp ~/.vimrc         $CONF_DIR/vim/vimrc
-cp -r ~/.vim/*      $CONF_DIR/vim/vim/
-
